878ce73210818526c495c267764e359d3110536a
David Mosterd <david@codepress.nl>
2025-03-15 17:01:50 +0100

    naming, comments and scoping

--- src/MapRESTtoMCP.php
+++ src/MapRESTtoMCP.php
@@ -2,6 +2,7 @@
 
 namespace WP_CLI\AiCommand;
 
+use WP_CLI\AiCommand\MCP\Server;
 use WP_CLI\AiCommand\RESTControllerList\Whitelist;
 use WP_REST_Request;
 
@@ -38,41 +39,44 @@ class MapRESTtoMCP {
 		return str_replace( '/wp/v2/', '', $route );
 	}
 
-	public function map_rest_to_mcp( $server ) {
-		$whitelist = $this->whitelist->get();
+	public function map_rest_to_mcp( Server $mcp_server ) {
+		$allowed_list = $this->whitelist->get();
 
-		$routes = rest_get_server()->get_routes();
+		$server = rest_get_server();
+		$routes = $server->get_routes();
 
         foreach ( $routes as $route => $endpoints ) {
 			foreach ( $endpoints as $endpoint ) {
-				if ( ! isset( $whitelist[ $route ] ) ) {
-					continue; // Route not whitelisted.
+				// Only allowed routes
+				if ( ! isset( $allowed_list[ $route ] ) ) {
+					continue;
 				}
 
-				// Generate a tool name based on route and method (e.g., "GET_/wp/v2/posts")
+				// Generate a tool name off route e.g. /wp/v2/posts
 				$tool_name = sanitize_key($route);
 
 				foreach( $endpoint['methods'] as $method_name => $enabled ) {
-					if ( ! isset( $whitelist[ $route ][ $method_name ] ) ) {
-						continue; // Method not whitelisted.
+					// Only allowed methods
+					if ( ! isset( $allowed_list[ $route ][ $method_name ] ) ) {
+						continue;
 					}
 
 					$tool = [
 						'name' => $tool_name . '_' . strtolower( $method_name ),
-						'description' => $whitelist[ $route ][ $method_name ],
+						'description' => $allowed_list[ $route ][ $method_name ],
 						'inputSchema' => $this->args_to_schema( $endpoint['args'] ),
-						'callable' => function ( $inputs ) use ( $route, $method_name ){
-							$request = new WP_REST_Request( $method_name, $route );
+						'callable' => function ( $inputs ) use ( $route, $method_name, $server ){
+							$request = new WP_REST_Request( $method_name, $route  );
 							$request->set_body_params( $inputs );
 
-							$response = rest_get_server()->dispatch( $request );
+							$response = $server->dispatch( $request );
 
 							// TODO $embed parameter is forced to true now
-							return rest_get_server()->response_to_data( $response, true );
+							return $server->response_to_data( $response, true );
 						},
 					];
 
-					$server->register_tool( $tool);
+					$mcp_server->register_tool( $tool);
 				}
 
 			}
