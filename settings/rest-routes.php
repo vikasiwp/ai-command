fc55c8e84a74937fce191b92ed520495850fbb55
David Mosterd <david@codepress.nl>
2025-03-15 17:50:55 +0100

    Added some user routes

--- settings/rest-routes.php
+++ settings/rest-routes.php
@@ -1,16 +1,29 @@
 <?php
 
 return [
-	'/wp/v2/pages'      => [
+	'/wp/v2/pages'                => [
 		'GET'  => 'Get a list of pages',
 		'POST' => 'Create a new page',
 	],
-	'/wp/v2/posts'      => [
+	'/wp/v2/posts'                => [
 		'GET'  => 'Get a list of posts',
 		'POST' => 'Create a new post',
 	],
-	'/wp/v2/categories' => [
+
+	// Categories
+	'/wp/v2/categories'           => [
 		'GET'  => 'Get a list of categories',
 		'POST' => 'Create a new category',
+	],
+
+	// Users
+	'/wp/v2/users'                => [
+		'GET'  => 'Retrieve a list of users.',
+		'POST' => 'Create a new user.',
+	],
+	'/wp/v2/users/id' => [
+		'GET'    => 'Retrieve a specific user by ID.',
+		'POST'   => 'Update a specific user by ID.',
+		'DELETE' => 'Delete a specific user by ID.',
 	],
 ];
