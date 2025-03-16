2aede6732783f1b7a1e1c454e5d884e5f0086734
Joost de Valk <joost@altha.nl>
2025-03-16 15:39:19 +0100

    Add file and url tools (#31)
    * Rough create post command
    
    * intermediate commit
    
    * Move functions
    
    * Works?
    
    * Clean up
    
    * loop on methods
    
    * Added a smaller whitelist
    
    * stuff
    
    * remove param
    
    * DI
    
    * Better schema
    
    * whitespace
    
    * extract tool, use sanitize_key
    
    * naming, comments and scoping
    
    * First workign prototype
    
    * merge
    
    * WIP
    
    * allowed list naming
    
    * make the routes a list that is configuratble open/closed
    
    * Add required
    
    * small markup
    
    * Added some user routes
    
    * small todo
    
    * Make named variables in REST requests work
    
    * Added better debug output handling.
    
    * Moved to separate method
    
    * clean
    
    * make it work with multiple named variables
    
    * multiple matches in routes
    
    * Remove unneeded function
    
    * Ignore vscode folder
    
    * return
    
    * Dynamicly created tool descriptions.
    
    * Ignore the home route.
    
    * Housekeeping
    
    * Route information
    
    * Add a temporary limit on the number of tools
    
    * Add debug around skipping tools for registration
    
    * Better response output.
    
    * route information
    
    * cleanup
    
    * Add URL and file handling tools, in separate files
    
    ---------
    
    Co-authored-by: Jan-Willem Oostendorp <webmaster@janw.me>
    Co-authored-by: David Mosterd <david@codepress.nl>

---
+++ src/tools/file-tools.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace AiCommand\Tools;
+
+class FileTools {
+
+    private $server;
+
+	public function __construct( $server ) {
+        $this->server = $server;
+        $this->register_tools();
+	}
+
+    public function register_tools() {
+		$this->server->register_tool(
+			[
+				'name'        => 'write_file',
+				'description' => 'Writes a file.',
+				'inputSchema' => [
+					'type'       => 'object',
+					'properties' => [
+						'path'    => [
+							'type'        => 'string',
+							'description' => 'The path of the file to write.',
+						],
+						'content' => [
+							'type'        => 'string',
+							'description' => 'The content of the file to write.',
+						],
+					],
+					'required'   => [ 'path', 'content' ],
+				],
+				'callable'    => function ( $params ) {
+					$path = $params['path'];
+					$content = $params['content'];
+					return file_put_contents( $path, $content );
+				},
+			]
+		);
+
+		$this->server->register_tool(
+			[
+				'name'        => 'delete_file',
+				'description' => 'Deletes a file.',
+				'inputSchema' => [
+					'type'       => 'object',
+					'properties' => [
+						'path'    => [
+							'type'        => 'string',
+							'description' => 'The path of the file to delete.',
+						],
+					],
+					'required'   => [ 'path' ],
+				],
+				'callable'    => function ( $params ) {
+					$path = $params['path'];
+					return unlink( $path );
+				},
+			]
+		);
+
+		$this->server->register_tool(
+			[
+				'name'        => 'read_file',
+				'description' => 'Reads a file.',
+				'inputSchema' => [
+					'type'       => 'object',
+					'properties' => [
+						'path'    => [
+							'type'        => 'string',
+							'description' => 'The path of the file to read.',
+						],
+					],
+					'required'   => [ 'path' ],
+				],
+				'callable'    => function ( $params ) {
+					$path = $params['path'];
+					return file_get_contents( $path );
+				},
+			]
+		);
+
+		$this->server->register_tool(
+			[
+				'name'        => 'move_file',
+				'description' => 'Moves a file.',
+				'inputSchema' => [
+					'type'       => 'object',
+					'properties' => [
+						'path'    => [
+							'type'        => 'string',
+								'description' => 'The path of the file to move.',
+						],
+						'new_path' => [
+							'type'        => 'string',
+							'description' => 'The new path of the file.',
+						],
+					],
+					'required'   => [ 'path', 'new_path' ],
+				],
+				'callable'    => function ( $params ) {
+					$path = $params['path'];
+					$new_path = $params['new_path'];
+					return rename( $path, $new_path );
+				},
+			]
+		);
+
+		$this->server->register_tool(
+			[
+				'name'        => 'list_files',
+				'description' => 'Lists files in a directory.',
+				'inputSchema' => [
+					'type'       => 'object',
+					'properties' => [
+						'path'    => [
+							'type'        => 'string',
+								'description' => 'The path of the directory to list files from.',
+						],
+					],
+					'required'   => [ 'path' ],
+				],
+				'callable'    => function ( $params ) {
+					$path = $params['path'];
+					return scandir( $path );
+				},
+			]
+		);
+
+    }
+}
+
